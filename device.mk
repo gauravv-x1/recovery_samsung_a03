e2cefe2c71d6245073e7561e51bde32b0801df27
Gaurav Verma <itzgrv7@gmail.com>
2023-12-23 18:23:32 +0000

    specific changes

--- device.mk
+++ device.mk
@@ -6,3 +6,15 @@
 #
 
 LOCAL_PATH := device/samsung/a03
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+TARGET_OTA_ASSERT_DEVICE := a03
+
+TARGET_COPY_OUT_VENDOR := vendor
+
