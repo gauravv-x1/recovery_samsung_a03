bccfeea643f34d5c9a815959c254930500bae2de
Gaurav Verma <122146666+gauravv-x1@users.noreply.github.com>
2023-12-26 09:04:08 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,7 +10,8 @@ add_lunch_combo twrp_a03-userdebug
 add_lunch_combo twrp_a03-eng
 
 export OF_MAINTAINER="gauravv.x1"
-export OF_MAINTAINER_AVATAR="maintainer.png"
-export OF_FL_PATH1="/tmp/flashlight"
+export OF_MAINTAINER_AVATAR="device/samsung/a03/maintainer.png"
+export OF_FL_PATH1="/system/flashlight"
 export OF_FL_PATH2=""
+export OF_FLASHLIGHT_ENABLE=1
 export OF_HIDE_NOTCH := 1
