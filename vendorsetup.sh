df64061ddfd7339e3e0738af25da6628f0461710
Gaurav Verma <122146666+gauravv-x1@users.noreply.github.com>
2023-12-27 09:57:43 +0530

    wtf

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ export ALLOW_MISSING_DEPENDENCIES="true"
 
 ## OrangeFox
 export OF_MAINTAINER="gauravv.x1"
-export OF_MAINTAINER_AVATAR="device/samsung/a03/maintainer.png"
+export OF_MAINTAINER_AVATAR="$(pwd)/device/samsung/a03/maintainer.png"
 export OF_FL_PATH1="/system/flashlight"
 export OF_FL_PATH2=""
 export OF_FLASHLIGHT_ENABLE=1
