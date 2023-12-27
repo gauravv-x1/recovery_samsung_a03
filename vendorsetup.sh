09e193315eb9849897ba60aebf84c1a81a972c66
Gaurav Verma <122146666+gauravv-x1@users.noreply.github.com>
2023-12-27 09:51:45 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,10 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# For building with minimal manifest
+export ALLOW_MISSING_DEPENDENCIES="true"
 
-add_lunch_combo twrp_a03-user
-add_lunch_combo twrp_a03-userdebug
-add_lunch_combo twrp_a03-eng
 
+## OrangeFox
 export OF_MAINTAINER="gauravv.x1"
-export OF_MAINTAINER_AVATAR="$(pwd)/device/samsung/a03/maintainer.png"
+export OF_MAINTAINER_AVATAR="device/samsung/a03/maintainer.png"
 export OF_FL_PATH1="/system/flashlight"
 export OF_FL_PATH2=""
 export OF_FLASHLIGHT_ENABLE=1
-export OF_HIDE_NOTCH=1
