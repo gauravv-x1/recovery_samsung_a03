da697ba9ae5082a620f25bd304a689443a725596
Gaurav Verma <gv01112005@gmail.com>
2023-12-28 19:39:09 +0000

    adapt update from twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,3 @@ export ALLOW_MISSING_DEPENDENCIES="true"
 ## OrangeFox
 export OF_MAINTAINER="gauravv.x1"
 export OF_MAINTAINER_AVATAR="$(pwd)/device/samsung/a03/maintainer.png"
-export OF_FL_PATH1="/system/flashlight"
-export OF_FL_PATH2=""
-export OF_FLASHLIGHT_ENABLE=1
