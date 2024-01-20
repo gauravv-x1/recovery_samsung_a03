de6a198ba2efa6c4aee15a2f566c21cbb7e9ccdf
Gaurav Verma <122146666+gauravv-x1@users.noreply.github.com>
2024-01-20 08:04:36 +0530

    Update twrp_a03.mk

--- twrp_a03.mk
+++ twrp_a03.mk
@@ -6,16 +6,14 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a03 device
 $(call inherit-product, device/samsung/a03/device.mk)
-
-PRODUCT_PACKAGES += charger_res_images
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
 
