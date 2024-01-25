31e0f14deccfa78411e28a449b9b0dd3656bea64
gauravv-x1 <gv01112005@gmail.com>
2024-01-25 07:28:18 +0530

    rebrand

--- twrp_a03.mk
+++ twrp_a03.mk
@@ -7,15 +7,22 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from a03 device
+# Inherit device configuration
 $(call inherit-product, device/samsung/a03/device.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a03/recovery/root,recovery/root)
 
 PRODUCT_DEVICE := a03
 PRODUCT_NAME := twrp_a03
